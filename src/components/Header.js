439f9476164dd701e93394134a683381047b0ace
lisin <lisin@DESKTOP-P4T8JEF>
2025-09-28 10:39:26 +1000

    Tiles Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,14 @@ import logo from '../logo.svg';
 
 function Header() {
     return(
-         <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Props
-        </a>
+      <header className="App-header">
+          <div className="header-container">
+            <img src={logo} className="App-logo" alt="logo" />
+                   <form action="/search" method="GET" className="search-form">
+               <input type="search" name="q" placeholder="Поиск..." aria-label="Поиск" />
+               <button type="submit">Найти</button>
+             </form>
+         </div>
       </header>
     )
 }
--- src/components/People.js
+++ src/components/People.js
@@ -11,22 +11,27 @@ function People(props)
             <div className='name'>{props.human.name} {props.human.surname}</div>
             <div className = 'age'>{props.human.age}</div>
         </div> */}
+        <div className="people-container">
         {
-        Object.keys(props.db).map
-        (
-            element =>
-            {
-                return(
-                    <div className='card'>
-                        <img src={props.db[element].photo} alt="Photo" />
-                        <div className='name'>{props.db[element].name} {props.db[element].surname}</div>
-                        <div className='gender'><img src={props.db[element].pol === 'male' ? male : female} alt="" /></div>
-                        <div className='age'>{props.db[element].age}</div>
-                    </div>
-                )
-            }
-        )
+            Object.keys(props.db).map
+            (
+                (element, index) =>
+                {
+                    return(
+                        <div className='card'
+                             key={element}
+                             style={{ animationDelay: `${index * 0.1}s`}}
+                        > 
+                            <img src={props.db[element].photo} alt="Photo" />
+                            <div className='name'>{props.db[element].name} {props.db[element].surname}</div>
+                            <div className='gender'><img src={props.db[element].pol === 'male' ? male : female} alt="" /></div>
+                            <div className='age'>{props.db[element].age}</div>
+                        </div>
+                    )
+                }
+            )
         }
+        </div>
        </> 
     )
 }
