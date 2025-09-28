287d2cdda8fff9b91a6c809717b90848d8d09398
lisin <lisin@DESKTOP-P4T8JEF>
2025-09-28 11:52:14 +1000

    Change Link to a

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import './Nav.css';
-import {Link} from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function Nav(props)
 {
@@ -11,7 +11,7 @@ function Nav(props)
                     (
                         element => 
                         {
-                            return <li key={element}><Link to={props.navigation[element]}>{element}</Link></li>
+                            return <li key={element}><a href={`#${props.navigation[element]}`}>{element}</a></li>
                         }
                     )
                 }
